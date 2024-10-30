da714e0da5fdbab313cdb373059956de559f97dd
Sanskriti Kumari <sans090804@gmail.com>
2024-10-30 22:21:42 +0530

    Add File

---
+++ main.js
@@ -0,0 +1,30 @@
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+  };
+  
+  ScrollReveal().reveal(".header_image", {
+    ...scrollRevealOption,
+    origin: "right",
+  });
+
+  ScrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+
+  ScrollReveal().reveal(".header__content h2", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+
+  ScrollReveal().reveal(".header__content p", {
+    ...scrollRevealOption,
+    delay: 1500,
+  });
+
+  ScrollReveal().reveal(".header__content form", {
+    ...scrollRevealOption,
+    delay: 2000,
+  });
